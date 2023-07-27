1b5269605cfb11bb79929ca605b56f22b264adf9
rpatel747 <rushabh.patel747@gmail.com>
2023-07-27 17:05:13 -0400

    Moved challenge to completed folder

---
+++ Completed/newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,13 @@
+function submitClick() {
+
+    let signupForm = document.getElementById("sign-up-form");
+    let successCard = document.getElementById("success-message-card");
+
+    signupForm.style.visibility = "hidden";
+    successCard.style.visbility = "visible";
+    successCard.style.display = "block";
+
+
+
+
+}
