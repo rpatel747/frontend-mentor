4f2185cb89f058101216b02cf41ffe456110faf1
rpatel747 <rushabh.patel747@gmail.com>
2023-10-27 21:42:50 -0400

    Working on challenge 1

---
+++ 1-vite/src/Components/NavBar.tsx
@@ -0,0 +1,18 @@
+
+export default NavBar();
+
+function NavBar(){
+    return (
+        <div id="navBar">
+            <img src="../assets/images/logo.svg"> </img>
+            <ul>
+                <li>Home</li>
+                <li>New</li>
+                <li>Popular</li>
+                <li>Trending</li>
+                <li>Categories</li>
+            </ul>
+        </div>
+    )
+}
+
